e8007dde042c4a5cf4b71d7217f4e15b4ee46017
Chaitanya-exe <jeetkrishna3333@gmail.com>
2024-02-20 00:53:08 +0530

    pushing code

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use std::fs;
+
+fn main(){
+    let listener = TcpListener::bind("127.0.0.1:5000").unwrap();
+
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0;1024];
+    stream.read(&mut buffer).unwrap();
+    let req = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(req){
+        let contents = fs::read_to_string("index.html").unwrap();
+        println!("{}",contents);
+        let response = format!("HTTP/1.1 200 OK\r\ncontent-Length: {}\r\n\r\n{}",contents.len(), contents); 
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else{
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("HTTP/1.1 404 NOT FOUND\r\ncontent-Length: {}\r\n\r\n{}",contents.len(), contents);
+        stream.write(response.as_bytes()).unwrap();
+    }
+}
