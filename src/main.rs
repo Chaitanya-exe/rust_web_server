447cb6684f095cefbc41a0c055fef507e2787935
Chaitanya-exe <jeetkrishna3333@gmail.com>
2024-02-20 16:35:31 +0530

    improved imports

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::net::TcpListener;
-mod lib;
+mod connection_handler;
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:5000").unwrap();
 
     for stream in listener.incoming(){
         let stream = stream.unwrap();
-        lib::handle_connection(stream);
+        connection_handler::handle_connection(stream);
     }
 }
