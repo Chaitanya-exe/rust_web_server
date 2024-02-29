321ee618f9285819dccafc316d9990f8a010d40c
Chaitanya-exe <jeetkrishna3333@gmail.com>
2024-03-01 01:11:00 +0530

    updated threadpool

--- src/lib.rs
+++ src/lib.rs
@@ -9,33 +9,44 @@ pub fn handle_connection(mut stream: TcpStream){
     stream.read(&mut buffer).unwrap();
     let req = b"GET / HTTP/1.1\r\n";
     if buffer.starts_with(req){
+        
         let contents = fs::read_to_string("index.html").unwrap();
         let response = format!("HTTP/1.1 200 OK\r\ncontent-Length: {}\r\n\r\n{}",contents.len(), contents); 
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
+
     } else if buffer.starts_with(b"GET /sleep HTTP/1.1\r\n"){
+        
         let contents = fs::read_to_string("home.html").unwrap();
         let response = format!("HTTP/1.1 200 OK\r\ncontent-Length: {}\r\n\r\n{}",contents.len(), contents);
         thread::sleep(Duration::from_secs(10));
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
+
     } 
 
     else{
+        
         let contents = fs::read_to_string("404.html").unwrap();
         let response = format!("HTTP/1.1 404 NOT FOUND\r\ncontent-Length: {}\r\n\r\n{}",contents.len(), contents);
         stream.write(response.as_bytes()).unwrap();
+
     }
 }
 
 pub struct ThreadPool{
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool{
     pub fn new(size: usize)-> ThreadPool{
         assert!(size > 0);
-        ThreadPool
+        let mut thread = Vec::with_capacity(size);
+
+        for _ in 0..size{
+            workers.push(Worker::new(id)); 
+        }
+        ThreadPool {threads}
     }
     
     pub fn execute<F>(&self, f: F)
@@ -44,4 +55,16 @@ impl ThreadPool{
     {
          
     }   
+
+    struct Worker{
+        id: usize,
+        thread: thread::JoinHandle<()>
+    }
+
+    impl Worker{
+        fn new(id: usize) -> Worker{
+            let thread = thread::spawn(|| {});
+            Worker{id, thread}
+        }
+    }
 }
